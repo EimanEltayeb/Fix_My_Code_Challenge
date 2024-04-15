5065e9c904e85fce557b579d5d8f6638a03083b5
EimanEltayeb <em_moh092@hotmail.com>
2024-04-15 11:02:12 -0700

    gedgfs

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -19,22 +19,22 @@ class square():
     def area_of_my_square(self):
         """ Area of the square """
 
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
         """permitter"""
 
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
         """str"""
 
-        return "{}/{}".format(self.width, self.height)
+        return "{}".format(self.width)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
